938f524fd298a29a9116be2537c00ab43b826e16
Roman FILIP <dicky@email.cz>
2016-05-21 23:54:56 +0200

    Added more tests for IQ restaurant service

--- RFI.MenuCardsAggregator.Services.Tests/IqRestaurantServiceTest.cs
+++ RFI.MenuCardsAggregator.Services.Tests/IqRestaurantServiceTest.cs
@@ -20,28 +20,7 @@ namespace RFI.MenuCardsAggregator.Services.Tests
             Assert.IsNotNull(menuCard);
             Assert.AreEqual("IQ Restaurant", menuCard.RestaurantName);
             Assert.AreEqual(1, menuCard.DayMenus.Count);
-            Assert.AreEqual(new DateTime(2016, 5, 23), menuCard.DayMenus[0].Date);
-
-            Assert.AreEqual(18, menuCard.DayMenus[0].Foods.Count);
-
-            AssertFood("Polévka: 0,33l Podkrkonošské kyselo", 22, false, menuCard.DayMenus[0].Foods[0]);
-            AssertFood("Polévka: 0,33l Polévka z domácího kuřete a kukuřice", 22, false, menuCard.DayMenus[0].Foods[1]);
-            AssertFood("320G Špagety s rajčatovou omáčkou a slaninou, sypané parmazánem", 79, false, menuCard.DayMenus[0].Foods[2]);
-            AssertFood("320G Italské smetanové rizoto se zeleninou a kuřecím masem", 79, false, menuCard.DayMenus[0].Foods[3]);
-            AssertFood("PIZZA 1/4: UZENINOVÁ (tomato, sýr, šunka, čabajka, salám, cibule)", 49, false, menuCard.DayMenus[0].Foods[4]);
-            AssertFood("PIZZA: MEXICANA (tomato, sýr, slanina, kukuřice, fazole, mexické papričky jalapeňos)", 87, false, menuCard.DayMenus[0].Foods[5]);
-            AssertFood("100G Koprová omáčka, vařené vejce/hovězí vařené, brambor/knedlík", 79, false, menuCard.DayMenus[0].Foods[6]);
-            AssertFood("120G Krůtí steak, zeleninový salát s kuskusem", 84, false, menuCard.DayMenus[0].Foods[7]);
-            AssertFood("120G Marinovaní rošťáci z vepřového masa, bramborová kaše, okurka", 79, false, menuCard.DayMenus[0].Foods[8]);
-            AssertFood("150G Smažené rybí filé, bramborová kaše", 84, false, menuCard.DayMenus[0].Foods[9]);
-            AssertFood("320G Zapečený květák s rajčaty a sýrem gouda, brambory s máslem", 79, false, menuCard.DayMenus[0].Foods[10]);
-            AssertFood("100G Kuřecí kung-pao, nudle/rýže/steakové hranolky", 89, true, menuCard.DayMenus[0].Foods[11]);
-            AssertFood("150G Pikantní mexický hamburger s papričkami jalapeňos, steakové hranolky, zelenina", 105, true, menuCard.DayMenus[0].Foods[12]);
-            AssertFood("150G Marinovaný steak z tuňáka, vařené brambory, limetková omáčka", 139, true, menuCard.DayMenus[0].Foods[13]);
-            AssertFood("150G Kuřecí flautas (dvě sm. tortilly plněné špenátem, sýrem, kuřecím, čínské zelí, rajčata)", 109, true, menuCard.DayMenus[0].Foods[14]);
-            AssertFood("150G Steak z roštěné, špekové fazolky, česneková bageta", 139, true, menuCard.DayMenus[0].Foods[15]);
-            AssertFood("150G Marinovaný kuřecí steak v zakysané smetaně, tandoori a česneku, podávané s francouzskou čočkou, steakové hranolky", 109, true, menuCard.DayMenus[0].Foods[16]);
-            AssertFood("200G Kuřecí gyros, hranolky/čínské nudle/rýže, tzatziky", 99, true, menuCard.DayMenus[0].Foods[17]);
+            AssertMondayFoods(menuCard.DayMenus[0]);
         }
 
         [TestMethod]
@@ -54,11 +33,124 @@ namespace RFI.MenuCardsAggregator.Services.Tests
             Assert.IsNotNull(menuCard);
             Assert.AreEqual("IQ Restaurant", menuCard.RestaurantName);
             Assert.AreEqual(5, menuCard.DayMenus.Count);
-            Assert.AreEqual(new DateTime(2016, 5, 23), menuCard.DayMenus[0].Date);
-            Assert.AreEqual(new DateTime(2016, 5, 24), menuCard.DayMenus[1].Date);
-            Assert.AreEqual(new DateTime(2016, 5, 25), menuCard.DayMenus[2].Date);
-            Assert.AreEqual(new DateTime(2016, 5, 26), menuCard.DayMenus[3].Date);
-            Assert.AreEqual(new DateTime(2016, 5, 27), menuCard.DayMenus[4].Date);
+            AssertMondayFoods(menuCard.DayMenus[0]);
+            AssertTuesdayFoods(menuCard.DayMenus[1]);
+            AssertWednesdayFoods(menuCard.DayMenus[2]);
+            AssertThursdayFoods(menuCard.DayMenus[3]);
+            AssertFridayFoods(menuCard.DayMenus[4]);
+        }
+
+        private void AssertMondayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 23), dayMenu.Date);
+            Assert.AreEqual(18, dayMenu.Foods.Count);
+
+            AssertFood("Polévka: 0,33l Podkrkonošské kyselo", 22, false, dayMenu.Foods[0]);
+            AssertFood("Polévka: 0,33l Polévka z domácího kuřete a kukuřice", 22, false, dayMenu.Foods[1]);
+            AssertFood("320G Špagety s rajčatovou omáčkou a slaninou, sypané parmazánem", 79, false, dayMenu.Foods[2]);
+            AssertFood("320G Italské smetanové rizoto se zeleninou a kuřecím masem", 79, false, dayMenu.Foods[3]);
+            AssertFood("PIZZA 1/4: UZENINOVÁ (tomato, sýr, šunka, čabajka, salám, cibule)", 49, false, dayMenu.Foods[4]);
+            AssertFood("PIZZA: MEXICANA (tomato, sýr, slanina, kukuřice, fazole, mexické papričky jalapeňos)", 87, false, dayMenu.Foods[5]);
+            AssertFood("100G Koprová omáčka, vařené vejce/hovězí vařené, brambor/knedlík", 79, false, dayMenu.Foods[6]);
+            AssertFood("120G Krůtí steak, zeleninový salát s kuskusem", 84, false, dayMenu.Foods[7]);
+            AssertFood("120G Marinovaní rošťáci z vepřového masa, bramborová kaše, okurka", 79, false, dayMenu.Foods[8]);
+            AssertFood("150G Smažené rybí filé, bramborová kaše", 84, false, dayMenu.Foods[9]);
+            AssertFood("320G Zapečený květák s rajčaty a sýrem gouda, brambory s máslem", 79, false, dayMenu.Foods[10]);
+
+            AssertWeekMenu(dayMenu, 11);
+        }
+
+        private void AssertTuesdayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 24), dayMenu.Date);
+            Assert.AreEqual(18, dayMenu.Foods.Count);
+
+            AssertFood("Polévka: 0,33l Marocká harira", 22, false, dayMenu.Foods[0]);
+            AssertFood("Polévka: 0,33l Hovězí vývar s masem a vejcem", 22, false, dayMenu.Foods[1]);
+            AssertFood("320G Kuřecí stehna s cizrnou, italské těstoviny", 84, false, dayMenu.Foods[2]);
+            AssertFood("320G Penne s kuřecím masem, francouzskou zeleninou a jogurtovo-smetanovou omáčkou", 79, false, dayMenu.Foods[3]);
+            AssertFood("PIZZA 1/4: CALABRIANA (tomato, sýr, šunka)", 49, false, dayMenu.Foods[4]);
+            AssertFood("PIZZA: GARGANO (smetana, herkules, slanina, jarní cibulka, kukuřice)POLÉVKA ZDARMA", 87, false, dayMenu.Foods[5]);
+            AssertFood("320G Hrachová kaše s opečenou klobásou a vídeňskou cibulkou, chléb", 84, false, dayMenu.Foods[6]);
+            AssertFood("120G Kuřecí prsa plněná ricotou , rukolový salát s ředkví, hranolky/rýže", 89, false, dayMenu.Foods[7]);
+            AssertFood("120G Vepřová panenka se špenátem a mozzaellou v listovém těst, šťouchané brambory sypané sušenými rajčaty a balkánským sýrem", 95, false, dayMenu.Foods[8]);
+            AssertFood("100G Štěpánská hovězí pečeně, rýže/hranolky", 82, false, dayMenu.Foods[9]);
+            AssertFood("100G Smažená sýrová jehla, vařené brambory/steakové hranolky, tatarka", 84, false, dayMenu.Foods[10]);
+
+            AssertWeekMenu(dayMenu, 11);
+        }
+
+        private void AssertWednesdayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 25), dayMenu.Date);
+            Assert.AreEqual(19, dayMenu.Foods.Count);
+
+            AssertFood("Polévka: 0,33l Staročeská houbová omáčka s bramborami", 22, false, dayMenu.Foods[0]);
+            AssertFood("Polévka: 0,33l Francouzská s mušličkami", 22, false, dayMenu.Foods[1]);
+            AssertFood("100G Grilovaný steak z lososa, bazalková omáčka, it. těstoviny, parmazán", 99, false, dayMenu.Foods[2]);
+            AssertFood("320G Gnocchi s houbovou omáčkou a kuřecím masem", 84, false, dayMenu.Foods[3]);
+            AssertFood("PIZZA 1/4: HUNGARIAN (tomato, sýr,čabajka, cibule)", 49, false, dayMenu.Foods[4]);
+            AssertFood("PIZZA: PRATALINO (tomato, mozzarella, šunka, žampiony, parmazán)", 99, false, dayMenu.Foods[5]);
+            AssertFood("130G Znojemské biftečky se sázeným vejce, rýže/steakové hranolky", 84, false, dayMenu.Foods[6]);
+            AssertFood("120G Smažená kuřecí prsa v parmazánovém těstíčku, bramborová kaše", 84, false, dayMenu.Foods[7]);
+            AssertFood("120G Milánská hovězí pečeně, těstoviny/rýže", 82, false, dayMenu.Foods[8]);
+            AssertFood("320G Risoto s kachním masem, zdobené rukolou", 89, false, dayMenu.Foods[9]);
+            AssertFood("320G Papas con chille (opečené brambůrky se sýrem, chilli papričkami a zeleninou)", 79, false, dayMenu.Foods[10]);
+            AssertFood("320G Švestkové knedlíky z bramborového těsta s cukrem a mákem", 79, false, dayMenu.Foods[11]);
+
+            AssertWeekMenu(dayMenu, 12);
+        }
+
+        private void AssertThursdayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 26), dayMenu.Date);
+            Assert.AreEqual(19, dayMenu.Foods.Count);
+
+            AssertFood("Polévka: 0,33l Čočková polévka s párkem", 22, false, dayMenu.Foods[0]);
+            AssertFood("Polévka: 0,33l Zeleninový vývar s celestýnskými nudlemi", 22, false, dayMenu.Foods[1]);
+            AssertFood("320G Kuřecí lasagne (česnek, zelenina, pomodoro, bazalka)", 84, false, dayMenu.Foods[2]);
+            AssertFood("320G Linguine s cuketou a chilli (česnek, chilli paprička, rajčata, bazalka)", 82, false, dayMenu.Foods[3]);
+            AssertFood("PIZZA 1/4: CALABRESE (šunka, anglická, paprika, olivy)", 49, false, dayMenu.Foods[4]);
+            AssertFood("PIZZA: CAPRICIOSA (tomato, šunka, žampiony)POLÉVKA ZDARMA", 87, false, dayMenu.Foods[5]);
+            AssertFood("120G Vídeňský hovězí plátek, restovaná cibulka, bramborová kaše", 84, false, dayMenu.Foods[6]);
+            AssertFood("240G Pečené kuřecí stehnýnko s mandlovou nádivkou, vařené brambory/rýže", 79, false, dayMenu.Foods[7]);
+            AssertFood("300G Steak z vepřové krkovice s grilovanou cibulkou, pečená skýva chleba", 95, false, dayMenu.Foods[8]);
+            AssertFood("100G Burrito de cerdo (kuřecí maso, omáčka mole poblano)", 79, false, dayMenu.Foods[9]);
+            AssertFood("320G Smažené žampiony, vařený brambor, tatarská omáčka", 79, false, dayMenu.Foods[10]);
+            AssertFood("150G Thajský salát s grilovaným kachním masem (kachní prsa, mix listových salátů, špenát, hrachové lusky, cherry rajčata, chilli papričky)", 119, false, dayMenu.Foods[11]);
+
+            AssertWeekMenu(dayMenu, 12);
+        }
+
+        private void AssertFridayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 27), dayMenu.Date);
+            Assert.AreEqual(18, dayMenu.Foods.Count);
+
+            AssertFood("Polévka: 0,33l Ukrajinský boršč", 25, false, dayMenu.Foods[0]);
+            AssertFood("Polévka: 0,33l Kuřecí vývar s masem a skleněnými nudlemi", 22, false, dayMenu.Foods[1]);
+            AssertFood("100G It. těstoviny s rukolovým pestem a kuřecími medailonky, parmazán", 84, false, dayMenu.Foods[2]);
+            AssertFood("320G Melanzana siciliana (gril. lilek plněný směsí bolognese, perlové cibulky, zapečené parmazánem) špecle 3ks", 84, false, dayMenu.Foods[3]);
+            AssertFood("PIZZA 1/4: FUMO PULCINO (smetana, uzený sýr, rajčata, olivy, šunka)", 49, false, dayMenu.Foods[4]);
+            AssertFood("PIZZA: AL CAPONE (šunka, kuřecí prsa, hermelín, slanina)", 87, false, dayMenu.Foods[5]);
+            AssertFood("120G Cikánská hovězí pečeně, rýže/brambory/těstoviny", 82, false, dayMenu.Foods[6]);
+            AssertFood("150G Smažený kuřecí řízek XXL, bramborová kaše", 79, false, dayMenu.Foods[7]);
+            AssertFood("120G Vepřový steak na grilu s česnekem a lutěnicí, steakové hranolky", 79, false, dayMenu.Foods[8]);
+            AssertFood("130G Drůbeží játra na cibulce, rýže/hranolky", 76, false, dayMenu.Foods[9]);
+            AssertFood("320G Zeleninové rizoto se žampiony, sýr gouda, salát", 79, false, dayMenu.Foods[10]);
+
+            AssertWeekMenu(dayMenu, 11);
+        }
+
+        private void AssertWeekMenu(DayMenu dayMenu, int startIndex)
+        {
+            AssertFood("100G Kuřecí kung-pao, nudle/rýže/steakové hranolky", 89, true, dayMenu.Foods[startIndex++]);
+            AssertFood("150G Pikantní mexický hamburger s papričkami jalapeňos, steakové hranolky, zelenina", 105, true, dayMenu.Foods[startIndex++]);
+            AssertFood("150G Marinovaný steak z tuňáka, vařené brambory, limetková omáčka", 139, true, dayMenu.Foods[startIndex++]);
+            AssertFood("150G Kuřecí flautas (dvě sm. tortilly plněné špenátem, sýrem, kuřecím, čínské zelí, rajčata)", 109, true, dayMenu.Foods[startIndex++]);
+            AssertFood("150G Steak z roštěné, špekové fazolky, česneková bageta", 139, true, dayMenu.Foods[startIndex++]);
+            AssertFood("150G Marinovaný kuřecí steak v zakysané smetaně, tandoori a česneku, podávané s francouzskou čočkou, steakové hranolky", 109, true, dayMenu.Foods[startIndex++]);
+            AssertFood("200G Kuřecí gyros, hranolky/čínské nudle/rýže, tzatziky", 99, true, dayMenu.Foods[startIndex]);
         }
 
         private void AssertFood(string expectedFoodName, decimal excpectedPrice, bool expectedIsWeekFood, Food actualFood)
