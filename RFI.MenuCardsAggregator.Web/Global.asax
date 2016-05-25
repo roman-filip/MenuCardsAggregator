747bc6839da2b2e4c6774114670ee80554fc7423
Roman FILIP <dicky@email.cz>
2016-05-25 16:12:42 +0200

    Added WEB project

---
+++ RFI.MenuCardsAggregator.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RFI.MenuCardsAggregator.Web.MvcApplication" Language="C#" %>
---
+++ RFI.MenuCardsAggregator.Web/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RFI.MenuCardsAggregator.Web.Startup))]
+namespace RFI.MenuCardsAggregator.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
