1c8739cfdc8994b2fe2a025ef916993646808239
Roman FILIP <dicky@email.cz>
2016-06-04 22:11:12 +0200

    Implementation of the SpielberkCafeRestaurantService

--- RFI.MenuCardsAggregator.Services.Tests/SpielberkCafeRestaurantServiceTest.cs
+++ RFI.MenuCardsAggregator.Services.Tests/SpielberkCafeRestaurantServiceTest.cs
@@ -1,14 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFI.MenuCardsAggregator.Services.Model;
+using RFI.MenuCardsAggregator.Services.Services;
 
 namespace RFI.MenuCardsAggregator.Services.Tests
 {
     [TestClass]
     public class SpielberkCafeRestaurantServiceTest : BaseRestaurantServiceTest
     {
+        [TestMethod]
+        public async Task WholeWeekMenuTest()
+        {
+            var data = File.ReadAllText(@"TestData\SpielberkCafeWholeWeekMenu.html");
+            IRestaurantService service = new SpielberkCafeRestaurantService(new HttpServiceMock(() => data));
+            var menuCard = await service.GetMenuCardAsync();
+
+            Assert.IsNotNull(menuCard);
+            Assert.AreEqual("Spielberk café", menuCard.RestaurantName);
+            Assert.AreEqual(5, menuCard.DayMenus.Count);
+
+            AssertMondayFoods(menuCard.DayMenus[0]);
+            AssertTuesdayFoods(menuCard.DayMenus[1]);
+            AssertWednesdayFoods(menuCard.DayMenus[2]);
+            AssertThursdayFoods(menuCard.DayMenus[3]);
+            AssertFridayFoods(menuCard.DayMenus[4]);
+        }
+
+        private void AssertMondayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 30), dayMenu.Date);
+            Assert.AreEqual(5, dayMenu.Foods.Count);
+        }
+
+        private void AssertTuesdayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 5, 31), dayMenu.Date);
+            Assert.AreEqual(6, dayMenu.Foods.Count);
+
+
+            // TODO - fix tuesday food count
+
+
+        }
+
+        private void AssertWednesdayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 6, 1), dayMenu.Date);
+            Assert.AreEqual(5, dayMenu.Foods.Count);
+        }
+
+        private void AssertThursdayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 6, 2), dayMenu.Date);
+            Assert.AreEqual(5, dayMenu.Foods.Count);
+        }
+
+        private void AssertFridayFoods(DayMenu dayMenu)
+        {
+            Assert.AreEqual(new DateTime(2016, 6, 3), dayMenu.Date);
+            Assert.AreEqual(5, dayMenu.Foods.Count);
+        }
     }
 }
--- RFI.MenuCardsAggregator.Services/Services/SpielberkCafeRestaurantService.cs
+++ RFI.MenuCardsAggregator.Services/Services/SpielberkCafeRestaurantService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using RFI.MenuCardsAggregator.Services.Model;
 
 namespace RFI.MenuCardsAggregator.Services.Services
@@ -23,9 +24,58 @@ namespace RFI.MenuCardsAggregator.Services.Services
             : base(httpService)
         { }
 
-        public override Task<MenuCard> GetMenuCardAsync()
+        public async override Task<MenuCard> GetMenuCardAsync()
         {
-            throw new NotImplementedException();
+            var menuCard = new MenuCard(RestaurantName, Uri);
+
+            var htmlDocument = await GetHtmlDocumentAsync();
+            var date = GetMondayDate(htmlDocument);
+
+            menuCard.DayMenus.Add(GetFoodForDay(htmlDocument, "Pondělí", date));
+            date = date.AddDays(1);
+
+            menuCard.DayMenus.Add(GetFoodForDay(htmlDocument, "Úterý", date));
+            date = date.AddDays(1);
+
+            menuCard.DayMenus.Add(GetFoodForDay(htmlDocument, "Středa", date));
+            date = date.AddDays(1);
+
+            menuCard.DayMenus.Add(GetFoodForDay(htmlDocument, "Čtvrtek", date));
+            date = date.AddDays(1);
+
+            menuCard.DayMenus.Add(GetFoodForDay(htmlDocument, "Pátek", date));
+
+            return menuCard;
+        }
+
+        private static DateTime GetMondayDate(HtmlDocument htmlDocument)
+        {
+            var dailyMenuNode = htmlDocument.DocumentNode.SelectSingleNode(".//p[text()='Denní menu']");
+            var dateNode = dailyMenuNode.ParentNode.NextSiblingElement().GetChildElements().First();
+            var dateStr = GetStringFomHtmlNode(dateNode);
+            var dateParts = dateStr.Split(' ');
+            var date = CreateDate(dateParts[0] + DateTime.Now.Year);
+
+            return date;
+        }
+
+        private DayMenu GetFoodForDay(HtmlDocument htmlDocument, string dayName, DateTime date)
+        {
+            var dayMenu = new DayMenu { Date = date };
+
+            var dayNode = htmlDocument.DocumentNode.SelectSingleNode(string.Format(".//p[text()='{0}']", dayName));
+
+            var pNode = dayNode.NextSiblingElement();
+            var nodeText = GetStringFomHtmlNode(pNode);
+            while (!string.IsNullOrEmpty(nodeText))
+            {
+                dayMenu.Foods.Add(new Food { Name = nodeText });
+
+                pNode = pNode.NextSiblingElement();
+                nodeText = GetStringFomHtmlNode(pNode);
+            }
+
+            return dayMenu;
         }
     }
 }
