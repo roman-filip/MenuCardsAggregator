92e28e360f97099356d2e5aeee236e8ccb9958f3
Roman FILIP <dicky@email.cz>
2018-05-04 21:59:13 +0200

    Removed old projects in .NET Framework

--- RFI.MenuCardsAggregator.Services_old/Services/IHttpService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace RFI.MenuCardsAggregator.Services.Services
-{
-    public interface IHttpService
-    {
-        Task<string> GetAsync(string uri);
-    }
-}
