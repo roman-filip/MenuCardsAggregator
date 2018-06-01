e1f218f07b087c41054b364b86676b6f47169361
Roman FILIP <dicky@email.cz>
2018-06-01 22:02:25 +0200

    Implementation of the TustoRestaurantService

---
+++ RFI.MenuCardsAggregator.Services.Tests/TustoRestaurantServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFI.MenuCardsAggregator.Services.Services;
+
+namespace RFI.MenuCardsAggregator.Services.Tests
+{
+    [TestClass]
+    public class TustoRestaurantServiceTest : BaseRestaurantServiceTest
+    {
+        [TestMethod]
+        public async Task WholeWeekMenuTest()
+        {
+            // TODO - extract common code from other test classes to the base class
+            var data = File.ReadAllText(@"TestData\TustoWholeWeekMenu.html");
+            IRestaurantService service = new TustoRestaurantService(new HttpServiceMock(() => data));
+            var menuCard = await service.GetMenuCardAsync();
+
+            var expectedMenuCard = LoadExpectedMenuCard(@"TestData\TustoWholeWeekMenu_result.json");
+
+            AssertMenuCard(expectedMenuCard, menuCard);
+        }
+    }
+}
---
+++ RFI.MenuCardsAggregator.Services/Services/TustoRestaurantService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using RFI.MenuCardsAggregator.Services.Model;
+
+namespace RFI.MenuCardsAggregator.Services.Services
+{
+    public class TustoRestaurantService : BaseRestaurantService
+    {
+        public override string RestaurantName => "Tusto restaurant";
+
+        public TustoRestaurantService()
+        {
+            Uri = "http://titanium.tusto.cz/tydenni-menu/";
+        }
+
+        public TustoRestaurantService(IHttpService httpService)
+            : base(httpService)
+        { }
+
+        public override Task<MenuCard> GetMenuCardAsync()
+        {
+            return Task.FromResult(new MenuCard(RestaurantName, Uri));
+        }
+    }
+}
