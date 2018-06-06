4945fe44d4174b8a4c5ce96e1051144e1a67dd46
Roman FILIP <dicky@email.cz>
2018-06-06 08:37:11 +0200

    Implementation of the TustoRestaurantService

--- RFI.MenuCardsAggregator.Services/Services/TustoRestaurantService.cs
+++ RFI.MenuCardsAggregator.Services/Services/TustoRestaurantService.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
 using RFI.MenuCardsAggregator.Services.Model;
 
 namespace RFI.MenuCardsAggregator.Services.Services
@@ -16,9 +20,41 @@ namespace RFI.MenuCardsAggregator.Services.Services
             : base(httpService)
         { }
 
-        public override Task<MenuCard> GetMenuCardAsync()
+        public override async Task<MenuCard> GetMenuCardAsync()
         {
-            return Task.FromResult(new MenuCard(RestaurantName, Uri));
+            var menuCard = new MenuCard(RestaurantName, Uri);
+            var htmlDocument = await GetHtmlDocumentAsync();
+
+            foreach (var dayTableNode in htmlDocument.DocumentNode.SelectNodes("*//table[@class='menu']"))
+            {
+                var dayMenu = GetMenuForDay(dayTableNode);
+                menuCard.DayMenus.Add(dayMenu);
+            }
+
+            return menuCard;
+        }
+
+        private static DayMenu GetMenuForDay(HtmlNode dayTableNode)
+        {
+            var dayMenu = new DayMenu
+            {
+                Date = GetDate(dayTableNode)
+            };
+            
+            foreach (var trNode in dayTableNode.GetChildElements())
+            {
+                
+            }
+
+            return dayMenu;
+        }
+
+        private static DateTime GetDate(HtmlNode dayTableNode)
+        {
+            var trNode = dayTableNode.GetChildElements().First();
+            var tdNode = trNode.GetChildElements().First();
+
+            return DateTime.MinValue;
         }
     }
 }
