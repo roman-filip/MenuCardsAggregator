f18d8ecf0a36e550cb2f929e226bfc3ee9f0f361
Roman FILIP <dicky@email.cz>
2018-06-07 22:30:18 +0200

    Implemented parsing of food name

--- RFI.MenuCardsAggregator.Services/Services/UEmilaRestaurantService.cs
+++ RFI.MenuCardsAggregator.Services/Services/UEmilaRestaurantService.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using RFI.MenuCardsAggregator.Services.Model;
 
 namespace RFI.MenuCardsAggregator.Services.Services
@@ -16,11 +19,26 @@ namespace RFI.MenuCardsAggregator.Services.Services
             : base(httpService)
         { }
 
-        public override Task<MenuCard> GetMenuCardAsync()
+        public override async Task<MenuCard> GetMenuCardAsync()
         {
             var menuCard = new MenuCard(RestaurantName, Uri);
+            var htmlDocument = await GetHtmlDocumentAsync();
 
-            return Task.FromResult(menuCard);
+            var dayMenu = new DayMenu { Date = DateTime.Now.Date }; //var htmlNodeCollection = htmlDocument.DocumentNode.SelectNodes("*//div[@class='date inner-layer']");
+
+            var notFoods = new List<string> { "Bezlepkové menu", "stálé menu", "Přílohový salát", "XXL menu příplatek (1/2 menu)", "Seznam alergenů na vyžádání u obsluhy", "Jídlo zakoupené s sebou uchovejte v teplotě do 6 C spotřebovat do 2 hodin", "Gramáž masa na porci je uvedena v syrovém stavu a v minimální hmotnosti" };
+            var foodDivNodes = htmlDocument.DocumentNode
+                .SelectNodes("*//div[@class='left-div item-name']")
+                .Where(d => !notFoods.Contains(d.InnerText.Trim()));
+            foreach (var foodDivNode in foodDivNodes)
+            {
+                var food = new Food();
+                dayMenu.Foods.Add(food);
+                System.Diagnostics.Debug.WriteLine(foodDivNode.InnerText);
+            }
+
+            menuCard.DayMenus.Add(dayMenu);
+            return menuCard;
         }
     }
 }
