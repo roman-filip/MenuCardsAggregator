6405f6d20bc46703cd60b105102d69ed7f788320
Roman FILIP <dicky@email.cz>
2018-06-07 21:31:46 +0200

    Created basic structure for U Emila restaurant

---
+++ RFI.MenuCardsAggregator.Services/Services/UEmilaRestaurantService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using RFI.MenuCardsAggregator.Services.Model;
+
+namespace RFI.MenuCardsAggregator.Services.Services
+{
+    public class UEmilaRestaurantService : BaseRestaurantService
+    {
+        public override string RestaurantName => "Restaurace U Emila";
+
+        public UEmilaRestaurantService()
+        {
+            Uri = "https://www.restauraceuemila.cz/dennimenu/";
+        }
+
+        public UEmilaRestaurantService(IHttpService httpService)
+            : base(httpService)
+        { }
+
+        public override Task<MenuCard> GetMenuCardAsync()
+        {
+            var menuCard = new MenuCard(RestaurantName, Uri);
+
+            return Task.FromResult(menuCard);
+        }
+    }
+}
