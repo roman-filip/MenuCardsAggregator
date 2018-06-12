7d96312370940ee4cab3fa62e66f31cc44f87d8e
Roman FILIP <dicky@email.cz>
2018-06-12 18:27:50 +0200

    Implemented parsing of date

--- RFI.MenuCardsAggregator.Services/Services/UEmilaRestaurantService.cs
+++ RFI.MenuCardsAggregator.Services/Services/UEmilaRestaurantService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using RFI.MenuCardsAggregator.Services.Model;
 
 namespace RFI.MenuCardsAggregator.Services.Services
@@ -24,7 +25,7 @@ namespace RFI.MenuCardsAggregator.Services.Services
             var menuCard = new MenuCard(RestaurantName, Uri);
             var htmlDocument = await GetHtmlDocumentAsync();
 
-            var dayMenu = new DayMenu { Date = DateTime.Now.Date }; //var htmlNodeCollection = htmlDocument.DocumentNode.SelectNodes("*//div[@class='date inner-layer']");
+            var dayMenu = new DayMenu { Date = GetDate(htmlDocument) };
 
             var notFoods = new List<string> { "Bezlepkové menu", "stálé menu", "Přílohový salát", "XXL menu příplatek (1/2 menu)", "Seznam alergenů na vyžádání u obsluhy", "Jídlo zakoupené s sebou uchovejte v teplotě do 6 C spotřebovat do 2 hodin", "Gramáž masa na porci je uvedena v syrovém stavu a v minimální hmotnosti" };
             var foodDivNodes = htmlDocument.DocumentNode
@@ -38,6 +39,13 @@ namespace RFI.MenuCardsAggregator.Services.Services
 
             menuCard.DayMenus.Add(dayMenu);
             return menuCard;
+        }
+
+        private static DateTime GetDate(HtmlDocument htmlDocument)
+        {
+            var dateParts = htmlDocument.DocumentNode.SelectNodes("*//div[@class='date inner-layer']")
+                .First().InnerText.Trim().Split(' ');
+            return CreateDate(dateParts[1], dateParts[2]);
         }
 
         private static string GetFoodName(string foodName) => foodName[1] == '.' ? foodName.Substring(3) : foodName;
