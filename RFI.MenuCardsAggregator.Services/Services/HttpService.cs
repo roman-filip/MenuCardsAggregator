2e590dec1a608163995af6a46895a783bdd9fc19
Roman FILIP <dicky@email.cz>
2018-07-11 02:44:04 +0200

    Disabled proxy for HttpClient

--- RFI.MenuCardsAggregator.Services/Services/HttpService.cs
+++ RFI.MenuCardsAggregator.Services/Services/HttpService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace RFI.MenuCardsAggregator.Services.Services
@@ -11,13 +9,7 @@ namespace RFI.MenuCardsAggregator.Services.Services
 
         public async Task<string> GetAsync(string uri)
         {
-            var config = new HttpClientHandler
-            {
-                UseProxy = true,
-                Proxy = new MyProxy("http://proxy.homecredit.cz:81")
-            };
-
-            using (var client = new HttpClient(/*config*/))
+            using (var client = new HttpClient(new HttpClientHandler { UseProxy = false }))
             {
                 var response = await client.GetAsync(uri);
                 response.EnsureSuccessStatusCode();
@@ -28,36 +20,5 @@ namespace RFI.MenuCardsAggregator.Services.Services
         }
 
         #endregion
-    }
-
-    public class MyProxy : IWebProxy
-    {
-        public MyProxy(string proxyUri)
-            : this(new Uri(proxyUri))
-        {
-        }
-
-        public MyProxy(Uri proxyUri)
-        {
-            this.ProxyUri = proxyUri;
-        }
-
-        public Uri ProxyUri { get; set; }
-
-        public ICredentials Credentials
-        {
-            get => null; // new NetworkCredential(@"", "");
-            set { }
-        }
-
-        public Uri GetProxy(Uri destination)
-        {
-            return this.ProxyUri;
-        }
-
-        public bool IsBypassed(Uri host)
-        {
-            return false; /* Proxy all requests */
-        }
     }
 }
